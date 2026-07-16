7e1f4ea2b25551d63329b564f769386e55edf4f9
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-07-15 21:08:15 -0600

    feat: Create consultas migration with fields for patient, dentist, appointment, and consultation details

---
+++ resources/js/Pages/AgendarCita/Calendario.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+import {Head} from "@inertiajs/vue3";
+</script>
+
+<template>
+<Head title="Calendario" />
+  <div class="">
+    <h1>
+      Calendario!!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
