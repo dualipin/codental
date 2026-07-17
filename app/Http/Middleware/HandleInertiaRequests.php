b599c4593559ea3623ab9703bb01e5dacc601625
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-07-17 09:26:38 -0600

    feat: remove unused middleware and views for cleaner codebase

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -14,7 +14,7 @@ class HandleInertiaRequests extends Middleware
      *
      * @var string
      */
-    protected $rootView = 'app';
+    protected $rootView = 'layouts.app-inertia';
 
     /**
      * Determines the current asset version.
